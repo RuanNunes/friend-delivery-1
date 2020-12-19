2a8f1f71c368a7c3fd956fde6857c4900376283b
RuanNunes <ruanschwanz@gmail.com>
2020-12-19 19:39:21 -0300

    import organize

--- src/main/java/br/com/frienddelivery/api/FriendsResource.java
+++ src/main/java/br/com/frienddelivery/api/FriendsResource.java
@@ -1,24 +1,16 @@
 package br.com.frienddelivery.api;
-
-import javax.validation.Valid;
-import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
-
-import br.com.frienddelivery.api.contract.FriendsApi;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import okhttp3.ResponseBody;
-
-import java.io.ObjectInputFilter.FilterInfo;
 import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
-import java.util.stream.Collector;
-
+import javax.validation.Valid;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import br.com.frienddelivery.api.contract.FriendsApi;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 /**
  * @author: Ruan Nunes
  * @version: 1.0 19/12/20
@@ -26,13 +18,6 @@ import java.util.stream.Collector;
 public class FriendsResource implements FriendsApi {
 
     private final Set<Friend> friends = Collections.newSetFromMap(Collections.synchronizedMap(new LinkedHashMap<>()));
-
-    public FriendsResource() {
-        friends.add(new Friend("Augusto Ribeiro", "augusto@email.com", "27098811725", "UE0J61AU8",
-                "Rua Alcino Pereira Neto, 550. Ed.Trinidad Ap.306. Bairro Jardim Camburi. ES",
-                List.of("pizza", "qualquer coisa com catupiri"), List.of("tomate", "cebola")));
-
-    }
 
     @Override
     public Set<Friend> get() {
