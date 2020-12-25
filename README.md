72e44c7fbc983b0571989d92088ee909f31178d8
Ruan Nunes <ruanschwanz@gmail.com>
2020-12-25 15:59:14 -0300

    create interface for doc open api  (#3)
    * create interface for doc open api
    
    * implements doc api
    
    * add lombok
    
    * import organize
    
    * fix
    
    * fix
    
    * teste
    
    * fix

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ This project uses Quarkus, the Supersonic Subatomic Java Framework.
 
 If you want to learn more about Quarkus, please visit its website: https://quarkus.io/ .
 
+This project aims to develop an api to develop an app for future app of deliverys friends.
+
 ## Running the application in dev mode
 
 You can run your application in dev mode that enables live coding using:
---
+++ src/main/java/br/com/frienddelivery/api/FriendsResource.java
@@ -0,0 +1,69 @@
+package br.com.frienddelivery.api;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import javax.validation.Valid;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import br.com.frienddelivery.api.contract.FriendsApi;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+/**
+ * @author: Ruan Nunes
+ * @version: 1.0 19/12/20
+ */
+public class FriendsResource implements FriendsApi {
+
+    private final Set<Friend> friends = Collections.newSetFromMap(Collections.synchronizedMap(new LinkedHashMap<>()));
+
+    @Override
+    public Set<Friend> get() {
+        return friends;
+    }
+
+    @Override
+    public Friend findByName(String name) {
+        return friends.stream().filter(friend -> friend.name.equals(name)).findFirst().orElse(null);
+    }
+
+    @Override
+    public Response create(@Valid Friend entity) {
+        if(Objects.nonNull(findByName(entity.name))){
+            return Response.status(Status.CONFLICT).build();
+        }
+        friends.add(entity);
+        return Response.ok(entity).build();    
+    }
+
+    @Override
+    public Response update(String name, @Valid Friend newEntity) {
+        if(Objects.isNull(findByName(name))){
+            throw new WebApplicationException("friend not found", Status.NOT_FOUND);
+        }
+        delete(name);
+        create(newEntity);
+        return Response.ok(newEntity).build();
+    }
+
+    @Override
+    public Response delete(String name) {
+        friends.removeIf(existingFriend ->
+        existingFriend.name.contentEquals(name));
+        return Response.ok(friends).build();
+    }
+
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Friend {
+        public String name;
+        public String email;
+        public String phone;
+        public String slack_id;
+        public String address;
+        public List<String> preferences;
+        public List<String> cant_be;
+    }
+}
